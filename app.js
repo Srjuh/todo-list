f682d5777b9d7f3888e9225290ff0fa9c327c78a
Sergio Neto <srg.a.neto@gmail.com>
2025-02-24 03:15:01 -0300

    Se for de primeira tem algo errado

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+let tasks = [];
+
+app.get('/', (req, res) => {
+  res.render('index', { tasks });
+});
+
+app.post('/add', (req, res) => {
+  const task = req.body.task;
+  if (task) {
+    tasks.push({ name: task, completed: false });
+  }
+  res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+  const taskIndex = req.body.taskIndex;
+  tasks.splice(taskIndex, 1);
+  res.redirect('/');
+});
+
+app.post('/toggle/:index', (req, res) => {
+  const taskIndex = req.params.index;
+  tasks[taskIndex].completed = !tasks[taskIndex].completed;
+  res.json({ success: true });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
